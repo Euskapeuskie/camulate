d680ac49562a57767412fd078c11a02a08feb5ad
Euskapeuskie <euskapeuskie@gmail.com>
2025-11-25 10:42:16 +0100

    Performance improvement if animation speed = 0.0;

--- src/cam_modeller/cam_system.rs
+++ src/cam_modeller/cam_system.rs
@@ -534,23 +534,16 @@ impl CamSystem {
 
     pub fn update_index(&mut self) -> () {
         let n_elements = self.follower.phis.len();
-        let index_next = self.index + ((self.rpm/3600.0)*n_elements as f64 * self.animation_speed/100.0) as usize;
-        
-        // Update the complete graph (whole 360° rotation) if animation speed is 0
-        if self.animation_speed == 0.0 {
-            for i in self.index..n_elements {
-                self.update_follower_position(i);
-                self.index = n_elements;
-            }
-        }
+        let index_next = match self.animation_speed {
+            0.0 => n_elements-1, // simulate everything at once if animation speed 0
+            _ => self.index + ((self.rpm/3600.0)*n_elements as f64 * self.animation_speed/100.0) as usize // next step depends on animation speed
+        };
 
         // Simulate only the next number of steps
-        else {
-            for i in self.index..=index_next {
-                self.update_follower_position(i & n_elements);
-                self.index = (i+1) % n_elements;
-            };
-        }
+        for i in self.index..index_next {
+            self.update_follower_position(i % n_elements);
+        };
+        self.index = index_next % n_elements;
 
     }
 
